9848fb038690382cc005d99347aa923b4f62834f
Hans-Gonzalez <110706345+Hans-Gonzalez@users.noreply.github.com>
2022-09-15 20:05:29 -0300

    Encrypting  functionality and display the output

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-var results = document.getElementById("encrypter-results");
-var encryptButton = document.getElementById("encrypt-button") ;
 
+var textInput = document.getElementById("text-area");
+var encryptButton = document.getElementById("encrypt-button");
+var outputArea = document.getElementById("encrypter-results");
+
+var text = textInput.value;
+const chars = {"a":"ai","e":"enter","i":"imes","o":"ober","u":"ufar"} 
+var output = encrypt();
+//var decryptOutput = decrypt();
+
+function encrypt(){
+     return text.replace(/[aeiou]/g, m => chars[m]); 
+}
 
 function write(){
-    results.textContent= "works";
+    encrypt;
+    outputArea.innerHTML = output;
+    textInput.value="";
 }
 
 
 encryptButton.onclick = write;
+
 
