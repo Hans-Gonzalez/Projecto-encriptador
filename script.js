38abc7ad4c455be83e715cebfbd84f1c92057a0e
Hans-Gonzalez <110706345+Hans-Gonzalez@users.noreply.github.com>
2022-09-16 12:45:24 -0300

    Purging Js & finish mobile CSS

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
 
-var textInput = document.getElementById("text-area");
-var encryptButton = document.getElementById("encrypt-button");
-var outputArea = document.getElementById("encrypter-results");
-
-var text = textInput.value;
-const chars = {"a":"ai","e":"enter","i":"imes","o":"ober","u":"ufar"} 
-var output = encrypt();
-//var decryptOutput = decrypt();
-
-function encrypt(){
-     return text.replace(/[aeiou]/g, m => chars[m]); 
-}
-
-function write(){
-    encrypt;
-    outputArea.innerHTML = output;
-    textInput.value="";
-}
-
-
-encryptButton.onclick = write;
 
 
